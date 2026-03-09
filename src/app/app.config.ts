d1aa5ee47361317e3bdb816b90b89f00ec6cb792
Rexhin Hoxha <rexhin@icloud.com>
2026-03-09 19:52:38 +0100

    feat: Configure MSAL for Azure B2C in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection, inject } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  inject,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideApollo } from 'apollo-angular';
@@ -11,6 +15,33 @@ import { provideEffects } from '@ngrx/effects';
 import { AuthEffects } from './auth/store/auth.effects';
 import { authReducer } from './auth/store/auth.reducer';
 import { routes } from './app.routes';
+
+import {
+  MSAL_INSTANCE,
+  MsalService,
+  MsalGuard,
+  MsalBroadcastService,
+} from '@azure/msal-angular';
+
+import { PublicClientApplication } from '@azure/msal-browser';
+
+export function MSALInstanceFactory() {
+  const msalInstance = new PublicClientApplication({
+    auth: {
+      clientId: 'd1f9a695-3b94-4f9a-a0c6-5508c475df0b',
+      authority: 'https://Fruitlycustomers.ciamlogin.com/36a16460-9d65-47a5-9de5-338f2fca444a',
+      redirectUri: 'http://localhost:4200',
+    },
+    cache: {
+      cacheLocation: 'localStorage'
+    }
+  });
+
+  // MSAL v3+ requires this call. Without it, popups won't trigger.
+  msalInstance.initialize(); 
+  
+  return msalInstance;
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -26,5 +57,13 @@ export const appConfig: ApplicationConfig = {
     }),
     provideStore({ auth: authReducer }),
     provideEffects([AuthEffects]),
-  ]
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory,
+    },
+
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService,
+  ],
 };

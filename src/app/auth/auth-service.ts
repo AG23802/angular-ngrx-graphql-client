1a1ba6f872cd48234d34fb9b7f2d47535144a137
Rexhin Hoxha <rexhin@icloud.com>
2026-03-09 19:52:38 +0100

    refactor: Update AuthService to use MSAL for login

--- src/app/auth/auth-service.ts
+++ src/app/auth/auth-service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { MsalService } from '@azure/msal-angular';
 
 @Injectable({
   providedIn: 'root',
@@ -9,11 +10,13 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
   private fullUrl = environment.apiBaseUrl;
   private httpClient = inject(HttpClient);
+  private msalService = inject(MsalService);
 
-  login(username: string, password: string): Observable<any> {
-    return this.httpClient
-      .post(`${this.fullUrl}/api/v1/auth/login`, { username, password });
-  }
+  login() {
+  return this.msalService.loginPopup({
+    scopes: ['api://b1e2e9be-44cb-4659-bb52-74194a2e6089']
+  });
+}
 
   refreshToken(refreshToken: string): Observable<any> {
     return this.httpClient.post(`${this.fullUrl}/api/v1/auth/refresh`, { refreshToken });
